5e6a181227c37bb0f65bbde092dd67b036c332fb
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-07-07 16:27:31 -0600

    Agregamos nuevo proyecto
    Lanza el mismo problema de existing the same in the database

---
+++ ApiWithTablesCreated/NZWalks2/NZWalksAPI2/Data/NZWalksDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalksAPI2.Models.Domain;
+
+namespace NZWalksAPI2.Data
+{
+    public class NZWalksDbContext: DbContext
+    {
+        public NZWalksDbContext(DbContextOptions dbContextOptions): base(dbContextOptions)
+        {
+            
+        }
+
+        public DbSet<Difficulty> Difficulties { get; set; }
+
+        public DbSet<Region> Regions { get; set; }
+
+        public DbSet<Walks> Walks { get; set; }
+
+    }
+}
