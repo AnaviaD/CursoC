9460f51644eed78325cc88d34f4c0d90ad0feb42
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-07-11 14:38:30 -0600

    Agregamos Esqueleto para mercancias

---
+++ ApiMercas/StudentApi/StudentApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentApi.Models.Domain;
+
+namespace StudentApi.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options): base(options)
+        {
+
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ ApiMercas/StudentApi/StudentApi/Models/DTO/CreateCategoryRequestDto.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentApi.Models.DTO
+{
+    public class CreateCategoryRequestDto
+    {
+
+        public string Name { get; set; }
+
+        public string UrlHandle { get; set; }
+    }
+}
