f45709e8319610445226670c51f83b8260bfb97d
Enrique Alberto Navia <anavia_n@outlook.com>
2023-09-12 21:21:58 -0600

    Revisamos el manejo de Streams

---
+++ CBasico/CursoBasico/CursoBasico/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CursoBasico.Seccion13MemoryString;
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Memory String \n");
+        CursoMemString Curso = new CursoMemString();
+        Curso.Run();
+    }
+}
---
+++ CBasico/CursoBasico/CursoBasico/Seccion13MemoryString/CursoMemString.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoBasico.Seccion13MemoryString
+{
+    class CursoMemString
+    {
+        public void Run()
+        {
+            MemoryStream ms = new MemoryStream(150);
+
+            var capacidad = ms.Capacity;
+
+            var longitud = ms.Length;
+
+            var posicion = ms.Position;
+
+            Console.WriteLine("Capacidad del memoriString:  (bites del string)\n");
+            Console.WriteLine(capacidad + "\n");
+            Console.WriteLine("Longitud memoriString:       (tamaño usado)\n");
+            Console.WriteLine(longitud + "\n");
+            Console.WriteLine("posicion del memoriString:   (lugar donde se encuentra el bite actual)\n");
+            Console.WriteLine(posicion + "\n");
+
+            //Seek el primero es la distancia desde el punto de referencia donde quedara la nueva posicion
+            //Seek indica el punto de referencia que utilizaremos
+            //SeekOrigin tiene 3 valores posibles, Begin, Current y End
+
+            //ms.Seek(0, SeekOrigin.Begin);
+            //ms.Seek(10, SeekOrigin.Begin);
+            //ms.Seek(-10, SeekOrigin.Begin);
+
+            //byte[] buffer = new byte[50];
+
+            //ms.Read(buffer, 0, 5);
+            //ms.Close();
+
+            string miInformacion = string.Empty;
+            byte[] buffer = new byte[50];
+
+            //El usuario introduce datos
+            Console.WriteLine("Introduce una cadena menor a 50 caracteres");
+            miInformacion = Console.ReadLine();
+
+            ms.Write(ASCIIEncoding.ASCII.GetBytes(miInformacion), 0, miInformacion.Length);
+            ms.Seek(0, SeekOrigin.Begin);
+            ms.Read(buffer, 0, buffer.Length);
+
+            Console.WriteLine("Texto Original: \n");
+            Console.WriteLine(ASCIIEncoding.ASCII.GetString(buffer));
+
+            Console.WriteLine("Ahora me posicionare 5 caracteres y leer la cadena: \n");
+
+            //Se posiciona en la opsicion 10
+            ms.Seek(10, SeekOrigin.Begin);
+            //Empieza a leer desde donde dejamos el seek, hasta la posicion que decidimos abajo 0, 5
+            ms.Read(buffer, 0, 5);
+
+            Console.WriteLine(ASCIIEncoding.ASCII.GetString(buffer));
+
+            ms.Close();
+
+
+        }
+    }
+}
