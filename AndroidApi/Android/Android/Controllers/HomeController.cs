d6efcf880339e4b21d8bb599932bf783c142dce8
Enrique Alberto Navia <anavia_n@outlook.com>
2023-12-19 19:38:12 -0600

    Basic Api to test things

---
+++ AndroidApi/Android/Android/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Android.Controllers
+{
+    [ApiController]
+    [Route("android")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        [Route("users")]
+        public dynamic Homs()
+        {
+            //android/users
+            return new
+            {
+                nombre = "jesus",
+                edad = 33
+            };
+            
+        }
+    }
+}
