a8dc5a7c8e3319715b7975207dd2b95602a1570e
Enrique Alberto Navia <anavia_n@outlook.com>
2024-02-28 23:39:29 -0600

    Clases

---
+++ CBasico/CursoBasico/CursoBasico/Seccion09Clases/Clase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoBasico.Seccion09Clases
+{
+    internal class Clase
+    {
+        public int Campo;
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+
+        //Las propiedades readonly se le puede asignar valor SOLO en el constructor
+        public readonly string Identificador;
+
+        public Clase()
+        {
+            Identificador = "ABCD";
+            
+        }
+    }
+}
