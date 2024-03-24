9ead51ad5c4ace76b82a3277970fbcae028c141d
Enrique Alberto Navia <anavia_n@outlook.com>
2024-03-24 11:49:37 -0600

    Adding Buttons

--- ExcelFilesDir/ExcelFiles/excelFiles/excelFiles/Form1.Designer.cs
+++ ExcelFilesDir/ExcelFiles/excelFiles/excelFiles/Form1.Designer.cs
@@ -31,12 +31,15 @@
             lbltexto = new Label();
             textBox1 = new TextBox();
             button1 = new Button();
+            txtPredict = new TextBox();
+            btnAction = new Button();
+            lblResult = new Label();
             SuspendLayout();
             // 
             // lbltexto
             // 
             lbltexto.AutoSize = true;
-            lbltexto.Location = new Point(69, 36);
+            lbltexto.Location = new Point(69, 20);
             lbltexto.Name = "lbltexto";
             lbltexto.Size = new Size(48, 15);
             lbltexto.TabIndex = 0;
@@ -45,7 +48,7 @@
             // textBox1
             // 
             textBox1.AllowDrop = true;
-            textBox1.Location = new Point(76, 71);
+            textBox1.Location = new Point(69, 38);
             textBox1.Multiline = true;
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(653, 321);
@@ -55,19 +58,47 @@
             // 
             // button1
             // 
-            button1.Location = new Point(337, 415);
+            button1.Location = new Point(69, 387);
             button1.Name = "button1";
-            button1.Size = new Size(75, 23);
+            button1.Size = new Size(115, 37);
             button1.TabIndex = 2;
-            button1.Text = "Crear DATA";
+            button1.Text = "Crear Modelo";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
+            // 
+            // txtPredict
+            // 
+            txtPredict.Location = new Point(485, 395);
+            txtPredict.Name = "txtPredict";
+            txtPredict.Size = new Size(198, 23);
+            txtPredict.TabIndex = 3;
+            // 
+            // btnAction
+            // 
+            btnAction.Location = new Point(689, 395);
+            btnAction.Name = "btnAction";
+            btnAction.Size = new Size(75, 23);
+            btnAction.TabIndex = 4;
+            btnAction.Text = "Predict";
+            btnAction.UseVisualStyleBackColor = true;
+            // 
+            // lblResult
+            // 
+            lblResult.AutoSize = true;
+            lblResult.Location = new Point(493, 424);
+            lblResult.Name = "lblResult";
+            lblResult.Size = new Size(39, 15);
+            lblResult.TabIndex = 5;
+            lblResult.Text = "Result";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(lblResult);
+            Controls.Add(btnAction);
+            Controls.Add(txtPredict);
             Controls.Add(button1);
             Controls.Add(textBox1);
             Controls.Add(lbltexto);
@@ -82,5 +113,8 @@
         private Label lbltexto;
         private TextBox textBox1;
         private Button button1;
+        private TextBox txtPredict;
+        private Button btnAction;
+        private Label lblResult;
     }
 }
