065891a1df849406738ace929e4643891379d120
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2024-03-25 17:08:22 -0600

    daily

--- ExcelFilesDir/ExcelFiles/excelFiles/excelFiles/obj/Debug/net8.0-windows/excelFiles.AssemblyInfo.cs
+++ ExcelFilesDir/ExcelFiles/excelFiles/excelFiles/obj/Debug/net8.0-windows/excelFiles.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("excelFiles")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ead51ad5c4ace76b82a3277970fbcae028c141d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20b09fb766aa9d157e9c41b6f33dd1ac6f3d33c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("excelFiles")]
 [assembly: System.Reflection.AssemblyTitleAttribute("excelFiles")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
