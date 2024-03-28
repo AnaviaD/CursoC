cb67a894db1af2c7705ac6b5ecb4e093dd6eb3d3
Enrique Alberto Navia <anavia_n@outlook.com>
2024-03-27 22:35:08 -0600

    Add NZWalk proyect

---
+++ NZWalksDir/NZWalks/NZWalks/NZWalksAPI/Models/Domain/Walk.cs
@@ -0,0 +1,20 @@
+﻿namespace NZWalksAPI.Models.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double LengthInKm { get; set; }
+        public string? WalkImageUrl { get; set; }
+        
+        public Guid DifficultyId { get; set; }
+        public Guid RegionId { get; set; }
+
+
+        //Navegation properties
+        public Difficulty   DifficultyProp { get; set; }
+        public Region       RegionProp { get; set; }
+
+    }
+}
