ca35d12bcd62d8c28621f6e6ada20cd176555c53
Enrique Alberto Navia <anavia_n@outlook.com>
2024-03-28 18:36:04 -0600

    Add create method

---
+++ NZWalksDir/NZWalks/NZWalks/NZWalksAPI/Controllers/RegionsController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NZWalksAPI.Data;
+using NZWalksAPI.Models.Domain;
+using NZWalksAPI.Models.DTO;
+
+namespace NZWalksAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : Controller
+    {
+        private NZWalksDbContext _context;
+
+        public RegionsController(NZWalksDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            //Get Data From Database - Domain Models
+            var regions = _context.Regions.ToList();
+
+            //Map Ddomain models to Dtos
+            var regionDto = new List<RegionDto>();
+            foreach (var region in regions) 
+            {
+                regionDto.Add(new RegionDto()
+                {
+                    Id                  = region.Id,
+                    Name                = region.Name,
+                    Code                = region.Code,
+                    RegionImageUrl      = region.RegionImageUrl,
+                });
+            }
+
+            return Ok(regions);
+        }
+
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public IActionResult GetById([FromRoute] Guid id) 
+        {
+            var regionDomain = _context.Regions.FirstOrDefault(x => x.Id == id);
+
+            if (regionDomain == null)
+            {
+                return NotFound();
+            }
+
+            var regionDto = new RegionDto
+            {
+                Id                  = regionDomain.Id,
+                Name                = regionDomain.Name,
+                Code                = regionDomain.Code,
+                RegionImageUrl      = regionDomain.RegionImageUrl,
+            };
+
+            return Ok(regionDto);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] AddRegionRequestDto regionRequest) 
+        {
+            //Map or convert DTO to domain Model
+            var regionDomainModel = new Region 
+            {
+                Code                = regionRequest.Code,
+                Name                = regionRequest.Name,
+                RegionImageUrl      = regionRequest.RegionImageUrl,
+            };
+
+            //Use Domain Model to create Region
+            _context.Regions.Add(regionDomainModel);
+            _context.SaveChanges();
+
+            var regionDto = new RegionDto
+            {
+                Id                  = regionDomainModel.Id,
+                Name                = regionDomainModel.Name,
+                Code                = regionDomainModel.Code,
+                RegionImageUrl      = regionDomainModel.RegionImageUrl,
+
+            };
+
+            return CreatedAtAction(nameof(GetById), new {id = regionDto.Id}, regionDto);
+        }
+    }
+}
--- NZWalksDir/NZWalks/NZWalks/NZWalksAPI/obj/Debug/net8.0/NZWalksAPI.AssemblyInfo.cs
+++ NZWalksDir/NZWalks/NZWalks/NZWalksAPI/obj/Debug/net8.0/NZWalksAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZWalksAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+065891a1df849406738ace929e4643891379d120")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fd0db8cad4222ef0c10b0fe621ccd827e705af5")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZWalksAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZWalksAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
