fdc07debf31479c4625b1aba90945a4a9f312a67
Enrique Alberto Navia <anavia_n@outlook.com>
2024-04-20 05:05:04 -0600

    need a fix

--- StopWatchDir/StopWatc/StopWatch/StopWatch/obj/Debug/net8.0-windows/StopWatch.AssemblyInfo.cs
+++ StopWatchDir/StopWatc/StopWatch/StopWatch/obj/Debug/net8.0-windows/StopWatch.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StopWatch")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f98f74a80bb43d1d96b4628695e41098e52328c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("StopWatch")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StopWatch")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
