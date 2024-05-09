aaa7b1feffccb0c475e416f1ca36a84aed72d588
Enrique Alberto Navia <anavia_n@outlook.com>
2024-05-08 19:54:19 -0600

    Adding dependencis

--- WaifuGenDir/waifuGenerator/waifuGenerator/waifuGenerator/Helpers/SelEngine.cs
+++ WaifuGenDir/waifuGenerator/waifuGenerator/waifuGenerator/Helpers/SelEngine.cs
@@ -1,4 +1,5 @@
 ﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,26 @@ namespace waifuGenerator.Helpers
         public SelEngine()
         {
             //SeleniumManager sel = SeleniumManager.Cr
+            // Ruta al ejecutable del controlador de Chrome
+            var driverPath = @"<ruta_al_directorio_del_controlador_de_Chrome>";
+
+            // Inicializar el controlador de Chrome
+            var driver = new ChromeDriver(driverPath);
+
+            // Navegar a la página web
+            driver.Navigate().GoToUrl("https://www.openiapony.com");
+
+            // Encontrar el botón de inicio de sesión por su clase o por su texto
+            // Por ejemplo, si el botón tiene la clase "login-btn", puedes encontrarlo así:
+            // var loginButton = driver.FindElement(By.ClassName("login-btn"));
+            // O si el botón tiene el texto "Login":
+            var loginButton = driver.FindElement(By.XPath("//button[text()='Login']"));
+
+            // Hacer clic en el botón de inicio de sesión
+            loginButton.Click();
+
+            // Cerrar el navegador
+            driver.Quit();
         }
 
     }
