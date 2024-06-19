6bd38f4bc5ec9775e8cd0513cf2283ed6eb6d171
Enrique Alberto Navia <anavia_n@outlook.com>
2024-06-18 19:45:48 -0600

    Add sales by mach excer

---
+++ CBasico/CursoBasico/CursoBasico/HackerRank/salesByMach.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoBasico.HackerRank
+{
+    public class salesByMach
+    {
+        public int sockMerchant(int n, List<int> ar)
+        {
+            List<int> pairsHold = new List<int>();
+            int totalPairs = 0;
+
+            foreach (int sock in ar)
+            {
+                if (pairsHold.Contains(sock))
+                {
+                    totalPairs++;
+                    pairsHold.Remove(sock);
+                }
+                else
+                {
+                    pairsHold.Add(sock);
+                }
+            }
+
+            return totalPairs;
+        }
+    }
+}
