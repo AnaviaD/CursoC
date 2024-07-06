fbea029666d33c4fb54cb022837a948d98b8785c
Enrique Alberto Navia <anavia_n@outlook.com>
2024-07-05 18:46:15 -0600

    Advance of PickingNumbers

---
+++ CBasico/CursoBasico/CursoBasico/HackerRank/pickingNumbers.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoBasico.HackerRank
+{
+    public class pickingNumbers
+    {
+        public int pickingNumbersFunc(List<int> a)
+        {
+            List<int> subArrayA, banList = new List<int>();
+            Hashtable repeatT = new Hashtable();
+            int maxA = 0, maxB = 0;
+
+            foreach (int numb in a)
+            {
+                if (!banList.Contains(numb))
+                {
+                    int countF = 0;
+                    for (int i = 0; i < a.Count; i++)
+                    {
+                        if (a[i] == numb)
+                        {
+                            countF++;
+
+                            if (!banList.Contains(a[i]))
+                                banList.Add(a[i]);
+
+                        }
+                    }
+                    repeatT.Add(numb, countF);
+
+                }
+            }
+
+
+
+            return 4;
+
+        }
+    }
+}
