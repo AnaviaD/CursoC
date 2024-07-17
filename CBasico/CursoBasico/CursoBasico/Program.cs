d65807a3e2fe2eb6bbef3e7e9bd6162e5e70a418
Enrique Alberto Navia <anavia_n@outlook.com>
2024-07-16 20:41:22 -0600

    Clase-Estructura

--- CBasico/CursoBasico/CursoBasico/Program.cs
+++ CBasico/CursoBasico/CursoBasico/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using CursoBasico.BigO;
+using CursoBasico.POO_01.Clase_Estructura;
 using CursoBasico.Seccion09Clases;
 using CursoBasico.Seccion13MemoryString;
 using System;
@@ -8,22 +9,31 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        On bigO = new On();
-        bigO.OnExcercice1();
+        Point punto = new Point();
+        punto.X = 10;
+        punto.Y = 8;
+
+        PointStruc puntoS = new PointStruc();
+        puntoS.X = 10;
+        puntoS.Y = 8;
+
+        SumaClaseCoord(punto);
+        SumaStructCoord(puntoS);
+
+        Console.WriteLine($"Suma de coordenadas Clase X = {punto.X}:    Y = {punto.Y}");
+        Console.WriteLine($"Suma de coordenadas Struct X = {puntoS.X}:    Y = {puntoS.Y}");
+        Console.ReadKey();
     }
 
-    //Console.WriteLine("Memory String \n");
-    //CursoMemString Curso = new CursoMemString();
-    //Curso.Run();
+    public static void SumaClaseCoord(Point point)
+    {
+        point.X = point.X + 10;
+        point.Y = point.Y + 10;
+    }
 
-
-    //Console.WriteLine("Clases \n");
-    //Clase clase = new Clase();
-    //clase.Campo = 5;
-    //clase.Nombre = "Juanito";
-    //clase.Apellido = "El chiwa";
-    //clase.Trabajo = "chaqueto";
-
-    //Console.WriteLine("Memory String {0}    -   {1} -   {2}\n", clase.Campo, clase.Nombre, clase.Apellido);
-
+    public static void SumaStructCoord(PointStruc point)
+    {
+        point.X = point.X + 10;
+        point.Y = point.Y + 10;
+    }
 }
