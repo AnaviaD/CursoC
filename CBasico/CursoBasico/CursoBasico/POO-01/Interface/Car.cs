f5d220a604671c0b7d9a21fd9fc5cba240364c8d
Enrique Alberto Navia <anavia_n@outlook.com>
2024-07-20 19:07:43 -0600

    Interfaz

---
+++ CBasico/CursoBasico/CursoBasico/POO-01/Interface/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoBasico.POO_01.Interface
+{
+    public class Car : IEquatable<Car>, IComparation01
+    {
+        public bool Equals(Car obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int EsIgual(int numero)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int EsIgualCoche(int numero) 
+        { 
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CBasico/CursoBasico/CursoBasico/POO-01/Interface/IEquatable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoBasico.POO_01.Interface
+{
+    internal interface IEquatable<T>
+    {
+        bool    Equals(T obj);
+        int     EsIgual(int numero);
+    }
+
+    public interface IComparation01
+    {
+        int EsIgualCoche(int numero);
+    }
+}
