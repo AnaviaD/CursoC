3a026bbca127f28a9070ccaefbb29188e07b542e
Enrique Alberto Navia <anavia_n@outlook.com>
2024-07-24 23:37:39 -0600

    Using DTOs

--- MyPageApiDir/MyPageApi/myWebApi/myWebApi/Controllers/RegionsController.cs
+++ MyPageApiDir/MyPageApi/myWebApi/myWebApi/Controllers/RegionsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using myWebApi.Data;
 using myWebApi.Models.Domain;
+using myWebApi.Models.DTO;
 
 namespace myWebApi.Controllers
 {
@@ -20,11 +21,82 @@ namespace myWebApi.Controllers
         //GET All regions
         //GET: https://localhost:portnumber/api/regions
         [HttpGet]
-        public IActionResult GetAll() 
+        public IActionResult GetAll()
         {
+            //Get data from Database - Domain models
             var regions = dbContext.Regions.ToList();
 
-            return Ok(regions);
+            //Map Domain Models to Dtos
+            var regionsDTO = new List<RegionDTO>();
+
+            foreach (var region in regions)
+            {
+                regionsDTO.Add(new RegionDTO()
+                {
+                    Id = region.Id,
+                    Name = region.Name,
+                    Code = region.Code,
+                    RegionImageUrl = region.RegionImageUrl,
+                });
+
+            }
+
+            //Return DTOs
+            return Ok(regionsDTO);
         }
+
+
+        //GET Single Region (Get Region By ID)
+        //GET: https://localhost:portnumber/api/regions/{id}
+        [HttpGet]
+        [Route("{id:guid}")]
+        public IActionResult GetById([FromRoute] Guid id)
+        {
+            //Get region Domain model from Database
+            var region = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+
+            if (region == null)
+            {
+                return NotFound();
+            }
+
+            //Map Region Domain model to DTO
+            var regionsDTO = new RegionDTO
+            {
+                Id = region.Id,
+                Name = region.Name,
+                Code = region.Code,
+                RegionImageUrl = region.RegionImageUrl,
+            };
+
+            return Ok(regionsDTO);
+        }
+
+
+        [HttpPost]
+        public IActionResult Create([FromBody] RegionRequestDTO addRegionRequest)
+        { 
+            var regionDomainModel  = new Region
+            {
+                Name            =   addRegionRequest.Name,
+                Code            =   addRegionRequest.Code,
+                RegionImageUrl  =   addRegionRequest.RegionImageUrl,
+            };
+
+            dbContext.Regions.Add(regionDomainModel);
+            dbContext.SaveChanges();
+
+            //Map domain model back to DTO
+            var regionDTO = new RegionDTO
+            {
+                Id = regionDomainModel.Id,
+                Name = regionDomainModel.Name,
+                Code = regionDomainModel.Code,
+                RegionImageUrl = regionDomainModel.RegionImageUrl,
+            };
+
+            return CreatedAtAction(nameof(GetById), new { Id = regionDomainModel.Id }, regionDomainModel);
+        }
+
     }
 }
--- MyPageApiDir/MyPageApi/myWebApi/myWebApi/obj/Debug/net8.0/myWebApi.AssemblyInfo.cs
+++ MyPageApiDir/MyPageApi/myWebApi/myWebApi/obj/Debug/net8.0/myWebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d3b1cf2161fad79c8d08d548fd4459796c0cb2c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7278fb906daab4404e8f456139d47374922f590c")]
 [assembly: System.Reflection.AssemblyProductAttribute("myWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
