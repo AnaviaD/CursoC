96d8a8239b8210a435ab9403101ce41e613f9373
Enrique Alberto Navia <anavia_n@outlook.com>
2024-11-14 19:43:56 -0600

    Update Review

---
+++ CBasico/CursoBasico/CursoBasico/POO-04/ClasesOrientadoAObjetos/CuentaBancaria.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoBasico.POO_04.ClasesOrientadoAObjetos
+{
+    class CuentaBancaria
+    {
+        //La clase es la plantilla que contiene tanto variables como metodos que nos hacen cambiar 
+        //Los valores de esas variables
+
+
+        //Variables
+        public int saldo { get; set; }
+        public bool estado { get; set; }
+
+        //Metodo
+        public static void Retirada()
+        {
+
+        }
+
+        public static void Deposito() 
+        {
+
+        }
+
+        public static void MostrarSaldo()
+        {
+            Console.WriteLine(1456);
+        }
+
+    }
+}
--- CBasico/CursoBasico/CursoBasico/Program.cs
+++ CBasico/CursoBasico/CursoBasico/Program.cs
@@ -5,17 +5,19 @@ using CursoBasico.POO_01.ClaseAbstracta_Interfaz;
 using CursoBasico.POO_02.Encapsulacion;
 using CursoBasico.POO_02.Polimorfismo;
 using CursoBasico.POO_03.BasicShit;
+using CursoBasico.POO_04.ClasesOrientadoAObjetos;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        CuentaBancaria03 cuenta = new CuentaBancaria03();
+        CuentaBancaria cuenta = new CuentaBancaria();
+
         cuenta.saldo = 150;
+
         Console.WriteLine("Cuenta.Saldo \n");
         Console.WriteLine(cuenta.saldo);
         Console.WriteLine("Cuenta.MostrarSaldo \n");
-        Console.WriteLine(cuenta.MostrarSaldo());
 
     }
 
