79c85adc3e6ce50665c9bcce45087d3075085d20
Enrique Alberto Navia <anavia_n@outlook.com>
2024-11-14 21:09:40 -0600

    Encapsulacion Review

---
+++ CBasico/CursoBasico/CursoBasico/POO-04/ClasesOrientadoAObjetos/point04.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoBasico.POO_04.ClasesOrientadoAObjetos
+{
+    public class point04
+    {
+        //private int x;
+        //private int y;
+
+        public int x { get; private set; }
+        public int y { get; private set; }
+
+        public void SetX(int value)
+        {
+            if (x < 100){
+                this.x = value;
+            }else{
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public void SetY(int value)
+        {
+            if (x < 100)
+            {
+                this.y = value;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public int GetX()
+        {
+            return x;
+        }
+
+        public int GetY() 
+        {
+            return y;
+        }
+    }
+}
