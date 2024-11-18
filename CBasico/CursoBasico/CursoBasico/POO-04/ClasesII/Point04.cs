bff4e55073c10010a21911ae6c4bd4e56f8563ba
Enrique Alberto Navia <anavia_n@outlook.com>
2024-11-17 22:52:56 -0600

    add struct review

---
+++ CBasico/CursoBasico/CursoBasico/POO-04/ClasesII/Point04.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoBasico.POO_04.ClasesII
+{
+    public class Point04
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
---
+++ CBasico/CursoBasico/CursoBasico/POO-04/ClasesII/PointStruc04.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoBasico.POO_04.ClasesII
+{
+    public struct PointStruc04
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
