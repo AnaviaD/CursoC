64dfed35ce08e29a5b25db3b162bfd37e4860feb
Enrique Alberto Navia <anavia_n@outlook.com>
2024-11-19 21:12:20 -0600

    adding review

--- CBasico/CursoBasico/CursoBasico/POO-04/ClasesII/Point04.cs
+++ CBasico/CursoBasico/CursoBasico/POO-04/ClasesII/Point04.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CursoBasico.POO_01.Clase_Estructura;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,4 +12,33 @@ namespace CursoBasico.POO_04.ClasesII
         public int X { get; set; }
         public int Y { get; set; }
     }
+
+    //Point04 punto = new Point04();
+    //punto.X = 10;
+    //    punto.Y = 10;
+
+    //    PointStruc04 pointStruc04 = new PointStruc04();
+    //pointStruc04.X = 5;
+    //    pointStruc04.Y = 5;
+
+    //    SumaCoordenadas(punto);
+    //SumarCoordenadas(pointStruc04);
+
+    //Console.WriteLine($"Suma de coordenadas clase : x={punto.X}  y={punto.Y} ");
+    //    Console.WriteLine($"Suma de coordenadas struct : x={pointStruc04.X}  y={pointStruc04.Y} ");
+    //    Console.ReadKey();
+
+
+    //public static void SumaCoordenadas(Point04 punto)
+    //{
+    //    punto.X += 10;
+    //    punto.Y += 10;
+    //}
+    //public static void SumarCoordenadas(PointStruc04 punto)
+    //{
+    //    punto.X += 15;
+    //    punto.Y += 15;
+    //}
+
+
 }
--- CBasico/CursoBasico/CursoBasico/Program.cs
+++ CBasico/CursoBasico/CursoBasico/Program.cs
@@ -12,33 +12,6 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Point04 punto = new Point04();
-        punto.X = 10;
-        punto.Y = 10;
-
-        PointStruc04 pointStruc04 = new PointStruc04();
-        pointStruc04.X = 5;
-        pointStruc04.Y = 5;
-
-        SumaCoordenadas(punto);
-        SumarCoordenadas(pointStruc04);
-
-        Console.WriteLine($"Suma de coordenadas clase : x={punto.X}  y={punto.Y} ");
-        Console.WriteLine($"Suma de coordenadas struct : x={pointStruc04.X}  y={pointStruc04.Y} ");
-        Console.ReadKey();
 
     }
-
-    public static void SumaCoordenadas(Point04 punto)
-    {
-        punto.X += 10;
-        punto.Y += 10;
-    }
-    public static void SumarCoordenadas(PointStruc04 punto)
-    {
-        punto.X += 15;
-        punto.Y += 15;
-    }
-
-
 }
