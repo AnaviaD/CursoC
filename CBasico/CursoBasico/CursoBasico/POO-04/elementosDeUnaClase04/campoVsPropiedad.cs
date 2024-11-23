5ed715ee374848a8c3a67ea12505ab85dd490399
Enrique Alberto Navia <anavia_n@outlook.com>
2024-11-22 19:38:44 -0600

    Add property review

--- CBasico/CursoBasico/CursoBasico/POO-04/elementosDeUnaClase04/campoVsPropiedad.cs
+++ CBasico/CursoBasico/CursoBasico/POO-04/elementosDeUnaClase04/campoVsPropiedad.cs
@@ -8,5 +8,33 @@ namespace CursoBasico.POO_04.elementosDeUnaClase04
 {
     public class campoVsPropiedad
     {
+
+        // Campo
+        public string campo;
+
+        // Propiedad
+        private string _propiedad;
+        public string Propiedad
+        {
+            get { return _propiedad; }
+            set
+            {
+                // Lógica adicional, por ejemplo, validación
+                if (!string.IsNullOrEmpty(value))
+                {
+                    _propiedad = value;
+                }
+                else
+                {
+                    throw new ArgumentException("El valor no puede ser nulo o vacío.");
+                }
+            }
+        }
+
+        public campoVsPropiedad()
+        {
+            campo = "Valor inicial del campo";
+            Propiedad = "Valor inicial de la propiedad";
+        }
     }
 }
