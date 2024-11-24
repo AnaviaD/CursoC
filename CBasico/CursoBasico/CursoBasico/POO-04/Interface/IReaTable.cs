a7fef048c71ceb92f764a6f2ada9261dec587a1d
Enrique Alberto Navia <anavia_n@outlook.com>
2024-11-24 17:32:11 -0600

    Review Interfaz

---
+++ CBasico/CursoBasico/CursoBasico/POO-04/Interface/IReaTable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoBasico.POO_04.Interface
+{
+    internal interface IReaTable
+    {
+        bool Equals(Task obj);
+        int EsIgual(int numero);
+    }
+}
