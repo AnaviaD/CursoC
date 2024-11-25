7e64d66c560b5a0d264f1ab2cc839e1857cd2d6e
Enrique Alberto Navia <anavia_n@outlook.com>
2024-11-24 18:50:01 -0600

    add interfaz review

--- CBasico/CursoBasico/CursoBasico/POO-04/Interface/ejemplo04.cs
+++ CBasico/CursoBasico/CursoBasico/POO-04/Interface/ejemplo04.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace CursoBasico.POO_04.Interface
 {
-    internal class ejemplo04
+    public class ejemplo04 : IReaTable
     {
+        public bool Equals(Task obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int EsIgual(int numero)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
