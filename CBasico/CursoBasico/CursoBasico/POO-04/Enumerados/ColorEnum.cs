9d0bc35cebbe7294ebe0b96ee649bfea442621ff
Enrique Alberto Navia <anavia_n@outlook.com>
2024-12-05 00:59:42 -0600

    review enum

---
+++ CBasico/CursoBasico/CursoBasico/POO-04/Enumerados/ColorEnum.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoBasico.POO_04.Enumerados
+{
+   enum ColorE
+    {
+        Rojo,
+        Verde,
+        Azul
+    }
+
+    enum Aligment : sbyte
+    {
+        Left = -1,
+        Center = 0,
+        Right = 1,
+    }
+
+
+    /*
+    static void PrintColor(ColorE color)
+    {
+        switch (color) 
+        {
+            case ColorE.Rojo:
+                Console.WriteLine("Rojo");
+                break;
+            case ColorE.Verde:
+                Console.WriteLine("Verde");
+                break;
+            case ColorE.Azul:
+                Console.WriteLine("Azul");
+                break;
+            default:
+                Console.WriteLine("def");
+                break;
+        }
+    }
+
+      private static void Main(string[] args)
+    {
+        //Podemos obtener objeto y propiedad
+        ColorE c = ColorE.Rojo;
+        PrintColor(c);
+        PrintColor(ColorE.Azul);
+        PrintColor(ColorE.Verde);
+
+        //Nos deberia regresar el index del emnum
+        int i = (int)ColorE.Azul;
+        Console.WriteLine(i);
+        //Tambien nos deberia regresar el index enum
+        ColorE c2 = (ColorE)2;
+        Console.WriteLine(c2);
+
+        var alineacion = Aligment.Left;
+        Console.WriteLine(alineacion);
+
+
+    }
+
+     */
+}
--- CBasico/CursoBasico/CursoBasico/Program.cs
+++ CBasico/CursoBasico/CursoBasico/Program.cs
@@ -1,23 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-using CursoBasico.BigO;
-using CursoBasico.POO_01.Clase_Estructura;
-using CursoBasico.POO_01.ClaseAbstracta_Interfaz;
-using CursoBasico.POO_02.Encapsulacion;
-using CursoBasico.POO_02.Polimorfismo;
-using CursoBasico.POO_03.BasicShit;
-using CursoBasico.POO_04.ClasesII;
-using CursoBasico.POO_04.ClasesOrientadoAObjetos;
-using CursoBasico.POO_04.dynamics;
-using System.Reflection;
+﻿using CursoBasico.POO_04.Enumerados;
 
 public class Program
-{
-
+{  
     private static void Main(string[] args)
-    {
-        
-        
-
+    {     
     }
 
 }
